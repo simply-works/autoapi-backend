fcde2fd031a239efa70903cc79b9d4c84dc97727
Ashuverma3162 <ashuverma3162@gmail.com>
2019-08-12 15:16:28 +0530

    feat: structure for dynamically deployable lambda

---
+++ deployable-app/app/utils/constants.js
@@ -0,0 +1,11 @@
+'use strict'
+exports.constants = {
+    defaultServerResponse: {
+        status: 500,
+        message: 'Something went wrong',
+        body: {}
+    },
+    SERVER_ERROR: "Internal server error",
+    DEFAULT_ERROR: "Something went wrong",
+    FETCHED_RECORD: "Fetched records"
+};
