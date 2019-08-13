01007218a6b4f8b3be22bfbe66eb435e5c354119
Ashuverma3162 <ashuverma3162@gmail.com>
2019-08-13 11:10:44 +0530

    Feat: crud apis for tables

---
+++ app/api/lambdas/table/handler.js
@@ -0,0 +1,136 @@
+const tableService = require('../../services/tableService');
+const constants = require('../../../utils/constants').constants;
+
+
+module.exports.getTables = async (event, context) => {
+    try {
+        let allTables = await tableService.getTables();
+        console.log('tableCreate', allTables);
+        let body = {};
+        let statusCode = '';
+        if (allTables.body && allTables.body.length && allTables.body[0].id) {
+            statusCode = allTables.status;
+            body.message = allTables.message;
+            body.tables = allTables.body
+        }
+        else {
+            statusCode = 400;
+            body.message = "Record not found";
+        }
+        return {
+            statusCode,
+            body: JSON.stringify(body)
+        }
+    }
+    catch (error) {
+        return {
+            statusCode: error.status,
+            body: JSON.stringify(error.message ? error.message : constants.DEFAULT_ERROR)
+        }
+    }
+}
+
+module.exports.getTable = async (event, context) => {
+    try {
+        let result = await tableService.getTable(event.pathParameters);
+        console.log('result', result);
+        let statusCode = '';
+        let body = {};
+        if (result && result.body && result.body[0].id) {
+            body.message = result.message;
+            body.table = result.body;
+        }
+        else {
+            statusCode = 400;
+            body.message = "Unable to fetch";
+        }
+        return {
+            statusCode,
+            body: JSON.stringify(body),
+        }
+    }
+    catch (error) {
+        console.log("Error while fetching record", error);
+        return {
+            statusCode: error.status,
+            body: JSON.stringify(error.message),
+        }
+    }
+}
+
+module.exports.createTable = async (event, context) => {
+    try {
+        let createRecord = await tableService.createTable({}, {}, JSON.parse(event.body));
+        console.log('createRecord', createRecord);
+        let body = {};
+        let statusCode = '';
+        if (createRecord && createRecord.body && createRecord.body.id) {
+            body.createdRecord = createRecord.body;
+            statusCode = createRecord.status;
+            body.message = createRecord.message;
+        } else {
+            statusCode = 500;
+            body.message = "Error while creating record"
+        }
+        return {
+            statusCode,
+            body: JSON.stringify(body),
+        }
+    }
+    catch (error) {
+        return {
+            statusCode: error.status,
+            body: JSON.stringify(error.message)
+        }
+    }
+}
+
+
+
+module.exports.updateTable = async (event, context) => {
+    try {
+        let updateRecord = await tableService.updateTable(event.pathParameters, {}, JSON.parse(event.body));
+        console.log('tableUpdate', updateRecord);
+        let statusCode = '';
+        let body = {};
+        if (updateRecord && (updateRecord.status === 204)) {
+            statusCode = updateRecord.status;
+            body.message = updateRecord.message;
+        } else {
+            statusCode = updateRecord.status;
+            body.message = updateRecord.message;
+        }
+        console.log('body', body);
+        console.log('status', statusCode)
+        return {
+            statusCode,
+            body: JSON.stringify(body)
+        }
+    } catch (error) {
+        return {
+            statusCode: error.status,
+            body: JSON.stringify(error.message)
+        }
+    }
+}
+
+module.exports.deleteTable = async (event, context) => {
+    try {
+        let deletedTable = await tableService.deleteTable(event.pathParameters);
+        console.log('tableDeleted', deletedTable);
+        let body = {};
+        let statusCode = '';
+        statusCode = deletedTable.status;
+        body = deletedTable.message;
+        return {
+            statusCode,
+            body: JSON.stringify(body)
+        }
+    }
+    catch (error) {
+        return {
+            statusCode: error.status,
+            body: JSON.stringify(error.message)
+        }
+    }
+}
---
+++ app/api/services/tableService.js
@@ -0,0 +1,124 @@
+'use strict';
+
+const postgresHelper = require('../../db/postgresHelper');
+const constants = require('../../utils/constants').constants;
+/**
+ * Get Tables details from table.
+ */
+exports.getTables = async (path, query, body) => {
+	try {
+		let responseObj = {};
+		Object.assign(responseObj, constants.defaultServerResponse);
+		let results = await postgresHelper.findRecords('Table');
+		if (results && results.length) {
+			console.log('result', results);
+			responseObj.body = results;
+			responseObj.status = 200;
+			responseObj.message = constants.FETCHED_RECORD;
+		} else {
+			responseObj.message = "No record found";
+		}
+		return responseObj;
+	} catch (error) {
+		console.log('Error while getting tables', error);
+		return responseObj;
+	}
+}
+
+/**
+ * Get Tables details from table.
+ */
+exports.getTable = async (path, query, body) => {
+	let responseObj = {};
+	Object.assign(responseObj, constants.defaultServerResponse);
+	try {
+		let filter = {
+			id: path.id
+		}
+		let results = await postgresHelper.findRecords('Table', filter);
+		if (results && results.length) {
+			responseObj.status = 200;
+			responseObj.body = results;
+			responseObj.message = constants.FETCHED_RECORD;
+			return responseObj;
+		} else {
+			throw new Error("Unable to fetch record");
+		}
+	} catch (error) {
+		return responseObj;
+	}
+}
+/**
+ * Create table and save data in table.
+ */
+exports.createTable = async (path, query, body) => {
+	let responseObj = {};
+	Object.assign(responseObj, constants.defaultServerResponse);
+	try {
+		let createdTable = await postgresHelper.createRecord('Table', body);
+		console.log('createTable', createdTable);
+		if (createdTable && createdTable.id) {
+			responseObj.status = 201;
+			responseObj.message = "Created successfully";
+			responseObj.body = createdTable;
+		} else {
+			responseObj.message = "Unable to create";
+		}
+		return responseObj;
+	} catch (error) {
+		return responseObj;
+	}
+}
+
+/**
+ * Update table details
+ */
+exports.updateTable = async (path, query, body) => {
+	let responseObj = {};
+	Object.assign(responseObj, constants.defaultServerResponse);
+	try {
+		let data = body;
+		let filter = {
+			id: path.id
+		}
+		let result = await postgresHelper.updateRecord('Table', data, filter);
+		if (result.indexOf(0) === 0) {
+			responseObj.message = "Record not found";
+			responseObj.status = 404;
+		} else {
+			responseObj.message = "Record updated successfully"
+			responseObj.status = 204;
+		}
+		return responseObj;
+	} catch (error) {
+		console.log('Error while updating table', error);
+		return responseObj;
+	}
+}
+
+/**
+ * Delete table information.
+ */
+exports.deleteTable = async (path, query, body) => {
+	let responseObj = {};
+	Object.assign(responseObj, constants.defaultServerResponse);
+	try {
+		let filter = {
+			id: path.id
+		}
+		let result = await postgresHelper.deleteRecord('Table', filter);
+		console.log('result', result);
+
+		if (result && (result !== 0)) {
+			responseObj.status = 204;
+			responseObj.message = "Deleted successfully";
+		} else {
+			responseObj.status = 404;
+			responseObj.message = "Record not found";
+		}
+		return responseObj;
+	} catch (error) {
+		console.log('Error while deleting table', error);
+		return responseObj;
+	}
+}
